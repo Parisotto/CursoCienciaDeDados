4bc2bc054356d32485e4a6402f5471050b1a570a
Parisotto <parisotto@gmail.com>
2024-08-13 08:16:59 -0300

    .

--- a016-import.py
+++ a016-import.py
@@ -1,5 +1,6 @@
 import math as m
 from pandas import DataFrame
+import scipy
 
 raiz = m.sqrt(9)
 print(f"A raiz quadrada é {raiz}")
@@ -9,3 +10,7 @@ print(m.ceil(6.1))
 dados = {'Nome': ['Alice','Beto','Carlos'],'Idade':[25,30,35]}
 dadosFormatados = DataFrame(dados)
 print(dadosFormatados)
+
+# PARA INSTALAR O PANDAS E O SCIPY DIGITE NO TERMINAL:
+# pip download pandas 
+# pip download scipy
