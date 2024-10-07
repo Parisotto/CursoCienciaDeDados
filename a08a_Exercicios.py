a2eb041a0e1371335f342df4e0ca591092490c26
Parisotto <parisotto@gmail.com>
2024-10-07 13:18:20 -0300

    .

--- a08a_Exercicios.py
+++ a08a_Exercicios.py
@@ -5,21 +5,37 @@ def calcularTempo(anos):
     minutos = horas * 60
     segundos = minutos * 60
 
-    print(f"{anos} anos equivalem a:")
-    print(f"    {meses} meses")
-    print(f"    {dias} dias")
-    print(f"    {horas} horas")
-    print(f"    {minutos} minutos")
-    print(f"    {segundos} segundos")
+    print(f"1) {anos} anos equivalem a:")
+    print(f"\t{meses} meses")
+    print(f"\t{dias} dias")
+    print(f"\t{horas} horas")
+    print(f"\t{minutos} minutos")
+    print(f"\t{segundos} segundos")
     
 def calcularTempo2(anos):
-    print(f"{anos} anos equivalem a:")
-    print(f"    {anos * 12} meses")
-    print(f"    {anos * 12 * 30} dias")
-    print(f"    {anos * 12 * 30 * 24} horas")
-    print(f"    {anos * 12 * 30 * 24 * 60} minutos")
-    print(f"    {anos * 12 * 30 * 24 * 60 * 60} segundos")
+    print(f"2) {anos} anos equivalem a:")
+    print(f"\t{anos * 12} meses")
+    print(f"\t{anos * 12 * 30} dias")
+    print(f"\t{anos * 12 * 30 * 24} horas")
+    print(f"\t{anos * 12 * 30 * 24 * 60} minutos")
+    print(f"\t{anos * 12 * 30 * 24 * 60 * 60} segundos")
+
+def calcularTempo3(anos):
+    r = f"3) {anos} anos equivalem a:\n"
+    r += f"\t{anos * 12} meses\n"
+    r += f"\t{anos * 12 * 30} dias\n"
+    r += f"\t{anos * 12 * 30 * 24} horas\n"
+    r += f"\t{anos * 12 * 30 * 24 * 60} horas\n"
+    r += f"\t{anos * 12 * 30 * 24 * 60 * 60} horas"
+    return r
+
 
 calcularTempo(2)
-print()    
-calcularTempo2(3)   
+print()
+
+anos = int(input("Digite quantos anos: "))   
+calcularTempo2(anos)   
+print()
+
+print(calcularTempo3(int(input("Digite quantos anos: "))))
+print()
