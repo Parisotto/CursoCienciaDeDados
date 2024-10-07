6ff67cdff22fa64de7f98faa0b02303adac6c0dd
Parisotto <parisotto@gmail.com>
2024-10-07 11:44:40 -0300

    .

--- a08a_Exercicios.py
+++ a08a_Exercicios.py
@@ -1,16 +1,25 @@
-def exercicio(anos):
+def calcularTempo(anos):
     meses = anos * 12
     dias = meses * 30
     horas = dias * 24
     minutos = horas * 60
     segundos = minutos * 60
 
-    print(f"{anos} equivalem a:")
+    print(f"{anos} anos equivalem a:")
     print(f"    {meses} meses")
     print(f"    {dias} dias")
     print(f"    {horas} horas")
     print(f"    {minutos} minutos")
     print(f"    {segundos} segundos")
     
-anos = int(input("Digite uma quantidade de anos: "))
-exercicio(anos)
+def calcularTempo2(anos):
+    print(f"{anos} anos equivalem a:")
+    print(f"    {anos * 12} meses")
+    print(f"    {anos * 12 * 30} dias")
+    print(f"    {anos * 12 * 30 * 24} horas")
+    print(f"    {anos * 12 * 30 * 24 * 60} minutos")
+    print(f"    {anos * 12 * 30 * 24 * 60 * 60} segundos")
+
+calcularTempo(2)
+print()    
+calcularTempo2(3)   
