e3144778962eca60455c46c497ae18d91758e8fb
Parisotto <parisotto@gmail.com>
2024-10-20 16:19:44 -0300

    .

--- a21-mp3.py
+++ a21-mp3.py
@@ -2,17 +2,24 @@ import random
 import pygame
 
 pygame.init()
-pygame.mixer.music.load('game.mp3')
+
+pygame.mixer.music.load('mp3/game.mp3')
 pygame.mixer.music.play()
 # input("Tocando... Tecle enter para parar.")
 
 mp3 = ['game', 'funny-bgm', 'space-station', 'stranger-things', 'this-8-bit-music']
 
-tocar = random.choice(mp3)
-titulo = tocar.split("-")
-titulo = " ".join(titulo).title()
+sair = ""
+while(sair != "s"):
+    tocar = random.choice(mp3)
+    titulo = tocar.split("-")
+    titulo = " ".join(titulo).title()
 
-pygame.mixer.music.load(f'{tocar}.mp3')
-pygame.mixer.music.play()
-input(f'Tocando "{titulo}"')
+    pygame.mixer.music.load(f'mp3/{tocar}.mp3')
+    pygame.mixer.music.play()
+    sair = input(f'Tocando "{titulo}".'
+                 f'\nTecle enter para tocar outra música aleatória.'
+                 f'\nDigite "s" para sair: ').lower().strip()
+    print()
+
 print("Bye bye...")
